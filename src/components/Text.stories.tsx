7fa812f5553fce8b6e36f55eae6c230fb53456a4
Guilherme Sandi <guilherme_sandi@hotmail.com>
2022-10-13 14:16:59 -0300

    Create Text component

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -5,8 +5,17 @@ export default {
   title: 'Components/Text',
   component: Text,
   args: {
-    children: 'Lorem ipsum.'
+    children: 'Lorem ipsum.',
+    size: 'md',
   },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio'
+      }
+    }
+  }
 } as Meta<TextProps>
 
 export const Default: StoryObj<TextProps> = {}
@@ -20,5 +29,21 @@ export const Small: StoryObj<TextProps> = {
 export const Large: StoryObj<TextProps> = {
   args: {
     size: 'lg'
+  }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <p>Testando</p>
+    )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      }
+    }
   }
 }
